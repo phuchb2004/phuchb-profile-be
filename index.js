3ff5364ca65ce15d4e13a5c7dbf6697ec1adf10f
PHUC\phuchb <phuchb04@gmail.com>
2025-12-19 20:27:14 +0700

    fix nodemailer config for cloud

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS

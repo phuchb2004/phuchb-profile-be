6bdde28227e54a89a431969335c83621a58d8e33
PHUC\phuchb <phuchb04@gmail.com>
2025-12-19 20:33:08 +0700

    change port nodemailer cloud

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: process.env.EMAIL_USER,

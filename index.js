11d202e4e804bd1b2c6b9851a095437c7fc069dc
PHUC\phuchb <phuchb04@gmail.com>
2025-12-21 18:11:48 +0700

    fix port for deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const Experience = require('./models/experience');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -93,6 +93,6 @@ app.get('/api/experience', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
